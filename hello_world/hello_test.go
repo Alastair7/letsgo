3cb50662fb8067c2ff86042cad7d44e94acfec58
Alastair7 <alexandru.ban.drmer@gmail.com>
2024-05-16 23:04:26 +0200

    hello_world | Refactor hello world program

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestHello(t *testing.T) {
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Alex");
+		got := Hello("Alex", "");
 		want := "Hello, Alex"
 	
 		if got != want {
@@ -16,8 +16,29 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run("say 'Hello, World when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, Worl"
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T){
+		got := Hello("Alex", "Spanish")
+		want := "Hola, Alex"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In French", func(t *testing.T) {
+		got := Hello("Alex", "French")
+		want := "Bonjour, Alex"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In Portuguese", func(t *testing.T) {
+		got := Hello("Alex", "Portuguese")
+		want := "Oi, Alex"
 
 		assertCorrectMessage(t, got, want)
 	})
